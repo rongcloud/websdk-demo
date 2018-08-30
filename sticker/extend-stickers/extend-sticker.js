acb1f2fe407f101fd56049639326742e09298b28
yuhongda <yuhongda@rongcloud.cn>
2018-08-30 17:51:44 +0800

    feat: 动态表情示例

---
+++ sticker/extend-stickers/extend-sticker.js
@@ -0,0 +1,95 @@
+var extendPackages = [{
+  id: 'Kdlak121b2jkdalkn',
+  icon: './extend-stickers/person/icon.png',
+  name: '人',
+  order: 11,
+  poster: '',
+  stickers: [{
+    packageId: 'Kdlak121b2jkdalkn',
+    stickerId: '1',
+    thumbUrl: './extend-stickers/person/1.gif',
+    url: './extend-stickers/person/1.gif',
+    desc: '摇摆',
+    height: 100,
+    width: 100
+  },{
+    packageId: 'Kdlak121b2jkdalkn',
+    stickerId: '2',
+    thumbUrl: './extend-stickers/person/2.png',
+    url: './extend-stickers/person/2.png',
+    desc: '喵星人',
+    height: 100,
+    width: 100
+  },{
+    packageId: 'Kdlak121b2jkdalkn',
+    stickerId: '3',
+    thumbUrl: './extend-stickers/person/3.png',
+    url: './extend-stickers/person/3.png',
+    desc: '爱上他',
+    height: 100,
+    width: 100
+  },{
+    packageId: 'Kdlak121b2jkdalkn',
+    stickerId: '4',
+    thumbUrl: './extend-stickers/person/4.png',
+    url: './extend-stickers/person/4.png',
+    desc: '打不过我',
+    height: 100,
+    width: 100
+  }]
+},{
+  id: 'Fkl12686yv4vmv4H9',
+  icon: './extend-stickers/mixed/icon.png',
+  name: '混合',
+  order: 10,
+  poster: '',
+  stickers: [{
+    packageId: 'Fkl12686yv4vmv4H9',
+    stickerId: '1',
+    thumbUrl: './extend-stickers/mixed/1.png',
+    url: './extend-stickers/mixed/1.png',
+    desc: '你丑',
+    height: 100,
+    width: 100
+  },{
+    packageId: 'Fkl12686yv4vmv4H9',
+    stickerId: '2',
+    thumbUrl: './extend-stickers/mixed/2.png',
+    url: './extend-stickers/mixed/2.png',
+    desc: '撩你',
+    height: 100,
+    width: 100
+  },{
+    packageId: 'Fkl12686yv4vmv4H9',
+    stickerId: '3',
+    thumbUrl: './extend-stickers/mixed/3.png',
+    url: './extend-stickers/mixed/3.png',
+    desc: '喵星人',
+    height: 100,
+    width: 100
+  },{
+    packageId: 'Fkl12686yv4vmv4H9',
+    stickerId: '4',
+    thumbUrl: './extend-stickers/mixed/4.png',
+    url: './extend-stickers/mixed/4.png',
+    desc: '卖了你',
+    height: 100,
+    width: 100
+  },{
+    packageId: 'Fkl12686yv4vmv4H9',
+    stickerId: '5',
+    thumbUrl: './extend-stickers/mixed/5.gif',
+    url: './extend-stickers/mixed/5.gif',
+    desc: '厉害',
+    height: 100,
+    width: 100
+  },{
+    packageId: 'Fkl12686yv4vmv4H9',
+    stickerId: '6',
+    thumbUrl: './extend-stickers/mixed/6.gif',
+    url: './extend-stickers/mixed/6.gif',
+    desc: '摇摆',
+    height: 100,
+    width: 100
+  }]
+}];
