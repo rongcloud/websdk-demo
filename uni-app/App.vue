8f3de81012744dd25e083b2d4356642d7366e73e
wangyonghao <wangyonghao@rongcloud.cn>
2019-04-09 12:55:13 +0800

    feat: add uni-app demo

---
+++ uni-app/App.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		onLaunch: function () {
+			console.log('App Launch')
+		},
+		onShow: function () {
+			console.log('App Show')
+		},
+		onHide: function () {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	body, html {
+		font-size: 20px;
+	}
+	input {
+		border: 1px solid black;
+	}
+	button {
+		display: inline-block;
+	}
+</style>
