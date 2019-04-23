669815b4a88149e85a71ccd97fad2c376a5bcb34
yuhongda <yuhongda@rongcloud.cn>
2019-04-23 18:21:38 +0800

    feat: 升级 RTC SDK 至 3.0.2

--- rongrtc/mock.js
+++ rongrtc/mock.js
@@ -1,8 +1,8 @@
 var App = {
-  appkey: 'c9kqb3rdkbb8j',
+  appkey: 'z3v5yqkbv8v30',
   roomId: 'room001',
   users: [
-    'fGf3CvgGGRV7jHnMYZGJunYNDAey+CFwhZPcG3x/Kb7zUM+9RGhoSsFCbCcopcqnYXRdZ6tmMDMPOM9QAQypjg==',
-    '9fEIrRmMdCeAAZ3u4YEFPcXZO74SCBcJ+lQ6rhLFlLuZ10eb7WRL78QdBwfPcA2KoXTjnj+hCRf+ilRfoG9tHQ=='
+    'HtWxw8/oF2iYL9Z2LrnG1LI6ZiT8q7s0UEaMPWY0lMwFumRQZRVL8Xr7Cjqr+FpABSbo+9EWQ/ZAP8UYCHF8meRNKy+58WBU',
+    'H7qhF5hGeLp7yDIzzvboMLI6ZiT8q7s0UEaMPWY0lMwFumRQZRVL8TgZaHy9Ji0CdlUwBFQRri/N9hFR9kc+muRNKy+58WBU'
   ]
 };
